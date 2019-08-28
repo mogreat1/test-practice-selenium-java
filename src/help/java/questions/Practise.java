64501cd04ffde5645e3189f1bae98167ec4fa1be
Max <mogreat1@gmail.com>
2019-08-28 12:53:28 +0200

    adf

--- src/help/java/questions/Practise.java
+++ src/help/java/questions/Practise.java
@@ -1,11 +1,14 @@
 package questions;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Practise {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String names[] = {"one", "two", "three", "one", "four", "five", "five"}; // Dublicate elements
         int a[] = {1, 2, 4, 5, 6, 7, 8}; // 1. largest/ smallest number 2. missing number
         String s = "@#$%@# latin string $34534f"; // remove junk
@@ -25,34 +28,15 @@ public class Practise {
         // repeated character in the string. Ex “assddd” -> 0,a; 1,s; 3, d
         String values = "assddd";
         Practise p = new Practise();
-        p.test(array);
+        var aaa = "String";
+        System.out.println(aaa);
 
     }
 
     public void test(int [] values) {
-        List<Integer> list = new ArrayList<>();
 
-        for (int i =0; i<values.length; i++){
-            int k=0;
-            if (!list.contains(values[i])){
-                list.add(values[i]);
-                k++;
-                for (int j=i+1; j<values.length; j++){
-                    if (values[i]==values[j]){
-                        k++;
-                    }
-                }
-                if (k==1){
-                    System.out.println(values[i]);
-                }
-            }
+
 
         }
-
-
-
-
-
-    }
 
 }
