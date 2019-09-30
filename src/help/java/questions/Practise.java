4b3e95e593bbdd7982e581591f63fc2159786052
mogreat1 <mogreat1@gmail.com>
2019-09-30 22:16:03 +0200

    adf

--- src/help/java/questions/Practise.java
+++ src/help/java/questions/Practise.java
@@ -14,34 +14,32 @@ import java.util.stream.Stream;
 public class Practise {
 
     String names[] = {"one", "two", "three", "one", "four", "five", "five"}; // Duplicate elements
-    int a[] = {1, 2, 4, 5, 6, 7, 8}; // 1. largest/ smallest number 2. missing number
+    int numbers[] = {1, 2, 4, 5, 6, 7, 8}; // 1. largest/ smallest number 2. missing number
     String s = "@#$%@# latin string $34534f"; // remove junk
     int x = 5; // swap values without third variable
     int y = 10;
     int num = 1234; // reverse number
-    String string = "madama"; // string can be read the same from both sides
+    String string = "madam"; // string can be read the same from both sides
     String str = "Selenium this is not Rest Assured"; // reverse string
     // 1.Print minimal number of the array
     // 2.Find max number in the column with min value of matrix
     int b[][] = {{-1, 6, 1, 4, 7}, {4, -2, -7, 88, 0}, {9, -3, 23, -31, 55}};
-    int array[] = {4, 5, 5, 5, 4, 6, 6, 9, 4, 88, 9, 4, 5, 66}; // Print unique number from the array
+    // Print unique number from the array OR Print True if sum of any two values equals to value
+    int array[] = {4, 5, 5, 5, 4, 6, 6, 9, 4, 88, 9, 4, 5, 66};  int value =75;
     int num1[] = {1, 3, 5, 7, 9}; // compare two arrays have the same values
-    int num2[] = {5, 1, 3, 9, 7, 66};//find value which doesn't exist in both arrays
+    int num2[] = {5, 1, 3, 9, 7, 66, 43};//find value which doesn't exist in both arrays
     int num3[] = {1, 3, 5, 7, 9};
     //Write a function which will return index and and value of each first
     // repeated character in the string. Ex “assddd” -> 0,a; 1,s; 3, d
     String values = "assddd";
     //Print Buzz for numbers which can be divided by 5, Fizz for those can be divided by 3, FizzBuzz by both
     ArrayList<String> list = new ArrayList<>(); //combine Strings but using Streams
-
     // list.add("one");
     //list.add("two");
+
     @Test
     private void test() {
-
     }
-
-
 }
 
 
