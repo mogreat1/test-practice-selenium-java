aec0367f598b5ebc6e3e4e9301ee66ceb0c67edc
mogreat1 <mogreat1@gmail.com>
2019-12-08 14:13:24 +0100

    practice

--- src/help/java/questions/Practise.java
+++ src/help/java/questions/Practise.java
@@ -19,19 +19,20 @@ public class Practise {
     int x = 5; // swap values without third variable
     int y = 10;
     int num = 1234; // reverse number
-    String string = "madam"; // string can be read the same from both sides
+    String string = "madas"; // string can be read the same from both sides
     String str = "Selenium this is not Rest Assured"; // reverse string
     // 1.Print minimal number of the array
     // 2.Find max number in the column with min value of matrix
     int b[][] = {{-1, 6, 1, 4, 7}, {4, -2, -7, 88, 0}, {9, -3, 23, -31, 55}};
     // Print unique number from the array OR Print True if sum of any two values equals to value
-    int array[] = {4, 5, 5, 5, 4, 6, 6, 9, 4, 88, 9, 4, 5, 66};  int value =75;
+    int array[] = {4, 5, 5, 5, 4, 6, 6, 9, 4, 88, 9, 4, 5, 66};
+    int value = 75;
     int num1[] = {1, 3, 5, 7, 9}; // compare two arrays have the same values
     int num2[] = {5, 1, 3, 9, 7, 66, 43};//find value which doesn't exist in both arrays
     int num3[] = {1, 3, 5, 7, 9};
     //Write a function which will return index and and value of each first
     // repeated character in the string. Ex “assddd” -> 0,a; 1,s; 3, d
-    String values = "assddd";
+    String values = "assddda";
     //Print Buzz for numbers which can be divided by 5, Fizz for those can be divided by 3, FizzBuzz by both
     ArrayList<String> list = new ArrayList<>(); //combine Strings but using Streams
     // list.add("one");
@@ -39,7 +40,11 @@ public class Practise {
 
     @Test
     private void test() {
+
+
     }
+
+
 }
 
 
